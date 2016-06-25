89dbc5ebc5f722ba2bfaaa72c3d023f22aeaf621
yeeptohh <yeept_ohh@hotmail.com>
2016-06-25 15:28:21 +0800

    renew

--- 爱服务/爱服务/Controllers/首页/DetailControllers/RechargeViewController.m
+++ 爱服务/爱服务/Controllers/首页/DetailControllers/RechargeViewController.m
@@ -363,11 +363,11 @@ static NSInteger i = 0;
                 NSString *orderSpec = [order description];
                 
                 //获取私钥并将商户信息签名,外部商户可以根据情况存放私钥和签名,只需要遵循RSA签名规范,并将签名字符串base64编码和UrlEncode
-                id<DataSigner> signer = CreateRSADataSigner(myPrivateKey);
-                
-                NSString *signedString = [signer signString:orderSpec];
-
-                NSLog(@"signedString1 = %@",signedString);
+//                id<DataSigner> signer = CreateRSADataSigner(myPrivateKey);
+//                
+//                NSString *signedString = [signer signString:orderSpec];
+//
+//                NSLog(@"signedString1 = %@",signedString);
                 
                 AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
                 
